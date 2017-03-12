73d73b49933cc39d2b46751ec569a93440ff8318
carlospanarello <carlospanarello@gmail.com>
2017-03-12 12:57:49 -0300

    5 commit

--- index.js
+++ index.js
@@ -21,27 +21,79 @@ var retornaDadosErro = function (res,err) {
 }
 
 restService.get('/graficoBarraVertical', function (req, res) {
-
     console.log('graficoBarraVertical');
-
     try {
         console.log('req-->' + req);
         console.log('req.body-->' + req.body);
         
         dados.barraVertical(function(resp){ 
             res.json(resp);
-            //res.send();
          });
-
     } catch (err) {
         console.error("Não foi possivel obter a informação", err);
         retornaDadosErro(res,err);
     }
 });
 
+restService.get('/graficoBarraHorizontal', function (req, res) {
+    console.log('graficoBarraHorizontal');
+    try {
+        console.log('req-->' + req);
+        console.log('req.body-->' + req.body);
+        
+        dados.barraHorizontal(function(resp){ 
+            res.json(resp);
+         });
+    } catch (err) {
+        console.error("Não foi possivel obter a informação", err);
+        retornaDadosErro(res,err);
+    }
+});
+
+restService.get('/calculo', function (req, res) {
+    console.log('calculo');
+    
+    for (var key in req.query) {
+        if (obj.hasOwnProperty(key)) {
+            var val = obj[key];
+            console.log(key + '-->' + val);
+        }
+    }
+});
+
+restService.get('/graficoPizza', function (req, res) {
+    console.log('graficoPizza');
+    try {
+        console.log('req-->' + req);
+        console.log('req.body-->' + req.body);
+        
+        dados.pizza(function(resp){ 
+            res.json(resp);
+         });
+    } catch (err) {
+        console.error("Não foi possivel obter a informação", err);
+        retornaDadosErro(res,err);
+    }
+});
+
+restService.get('/graficoLinha', function (req, res) {
+    console.log('graficoLinha');
+    try {
+        console.log('req-->' + req);
+        console.log('req.body-->' + req.body);
+        
+        dados.linha(function(resp){ 
+            res.json(resp);
+         });
+    } catch (err) {
+        console.error("Não foi possivel obter a informação", err);
+        retornaDadosErro(res,err);
+    }
+});
+
+
 restService.get('/',function(req,res){
 	res.end('Servidor On');
-	console.log('Entrou');
 });
 
 restService.listen((process.env.PORT || 5000), function () {
